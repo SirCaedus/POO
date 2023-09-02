b561b549f688fd28e1473a008f1ce1182c1873e2
Juan Martín Gonzalez Mansilla <111925841+SirCaedus@users.noreply.github.com>
2023-09-02 16:54:09 -0300

    Add files via upload

---
+++ TP1/Ej10/src/Main.java
@@ -0,0 +1,19 @@
+import Ej10.ListaDeTareas;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inicializando tareas.");
+
+        ListaDeTareas lista = new ListaDeTareas();
+        LocalDate hoy = LocalDate.now();
+        LocalDate ayer = hoy.minusDays(1);
+        LocalDate maniana = hoy.plusDays(1);
+
+        lista.agregarTarea("Baja","Incompleta","Ir al supermercado mañana.",maniana,hoy);
+        lista.agregarTarea("Baja","Completa","Consultar repuesto del auto.",hoy,hoy);
+        lista.agregarTarea("Baja","Completa","Ir al cine a ver la nueva película de Marvel.",ayer);
+
+        lista.mostrarTareas();
+    }
+}
